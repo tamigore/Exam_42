09b21609e98df3706d6c2d96a30e075592865388
Tadeo AMIGORENA <tamigore@e1r10p15.42.fr>
2021-12-02 19:12:40 +0100

    rank 3

--- Rank_3/mini_paint.c
+++ Rank_3/mini_paint.c
@@ -1,158 +1,170 @@
-#include "mini_paint.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
-int		ft_strlen(char *str)
+typedef struct s_bg
 {
-	int	i;
+	int	width;
+	int	height;
+	char	bgchar;
+	FILE	*file;
+	char	**matrix;
+}	t_bg;
+typedef struct s_circl
+{
+	char	type;
+	float	x;
+	float	y;
+	float	rad;
+	char	fillchar;
+}	t_circl;
 
-	i = 0;
+int	ft_strlen(char *str)
+{
+	int	i = 0;
+
+	if (!str)
+		return (i);
 	while (str[i])
 		i++;
 	return (i);
 }
 
-void	ft_putstr(char *str, int len)
+void	write_str_nl(char *str)
 {
-	int	i;
-
-	i = 0;
-	while (str[i] && i < len)
-		write(1, &str[i++], 1);
+	write(1, str, ft_strlen(str));
+	write(1, "\n", 1);
 }
 
-int		dist(float x, float y, float cx, float cy, char type)
+void	clear_data(t_bg *bg)
 {
-	float	dist;
+	int	i = -1;
 
-	dist = sqrtf((x - cx) * (x - cx) + (y - cy) * (y - cy));
-	if (dist <= 1)
+	if (bg->file)
+		fclose(bg->file);
+	if (bg->matrix)
 	{
-		if (type == 'C')
+		while (++i < bg->height)
+			free(bg->matrix[i]);
+		free(bg->matrix);
+	}
+}
+
+int	my_perror(t_bg *bg, char *errmess)
+{
+	write_str_nl(errmess);
+	clear_data(bg);
+	return (1);
+}
+
+int	parse_bg(t_bg *bg)
+{
+	int		scan_ret;
+	int		i = -1;
+
+	if ((scan_ret = fscanf(bg->file, "%d %d %c\n", &bg->width, &bg->height, &bg->bgchar)) != 3)
+		return (1);
+	if (bg->width <= 0 || bg->width > 300 || bg->height <= 0 || bg->height > 300
+		|| bg->bgchar < 32 || bg->bgchar > 126)
+		return (1);
+	if (!(bg->matrix = (char **)malloc(bg->height * sizeof(char *))))
+		return (1);
+	while (++i < bg->height)
+	{
+		if (!(bg->matrix[i] = (char *)calloc(bg->width + 1, sizeof(char))))
 			return (1);
-		else
+		memset(bg->matrix[i], bg->bgchar, bg->width);
+	}
+	return (0);
+}
+
+float	dist_to_centre(t_circl *c, float x, float y)
+{
+	float	dxsq = powf(c->x - x, 2);
+	float	dysq = powf(c->y - y, 2);
+
+	return (sqrtf(dxsq + dysq));
+}
+
+int	is_in_circle(t_circl *c, int x, int y)
+{
+	float dist = dist_to_centre(c, (float)x, (float)y);
+	if (dist <= c->rad)
+	{
+		if (dist <= c->rad - 1.00000000)
+			return (1); //inside
+		return (2); // border
+	}
+	return (0);
+}
+
+void	draw_circle(t_bg *bg, t_circl *c)
+{
+	int	x = -1;
+	int y = -1;
+	int	part_of;
+
+	while (++y < bg->height)
+	{
+		x = -1;
+		while (++x < bg->width)
 		{
-			if (dist <= 1 && dist >= 0.9)
-				return (1);
+			part_of = is_in_circle(c, x, y);
+			if ((c->type == 'c' && part_of == 2)
+				|| (c->type == 'C' && part_of))
+			{
+				bg->matrix[y][x] = c->fillchar;
+			}
+		}
+	}
+}
+
+int	parse_draw_circles(t_bg *bg)
+{
+	int	scan_ret;
+	t_circl	c;
+
+	while ((scan_ret = fscanf(bg->file, "%c %f %f %f %c\n", &c.type, &c.x, &c.y, &c.rad, &c.fillchar)) != EOF)
+	{
+		if (scan_ret == EOF)
+			break ;
+		if (scan_ret == 1 && c.type == '\n')
+			continue ;
+		if (scan_ret != 5 || (c.type != 'c' && c.type != 'C') || c.rad <= 0.00000000
+			|| c.fillchar < 32 || c.fillchar > 126)
+			return (1);
+		if (scan_ret == 5)
+		{
+			draw_circle(bg, &c);
 		}
 	}
 	return (0);
 }
 
-void	put_obj(t_env *env)
+void	print_result(t_bg *bg)
 {
-	int	x;
-	int	y;
+	int	i = -1;
 
-	y = 0;
-	while (y < env->draw.height)
-	{
-		x = 0;
-		while (x < env->draw.width)
-		{
-			if (dist(x, y, env->obj.x, env->obj.y, env->obj.type))
-				env->draw.matrice[x][y] = env->obj.color;
-			x++;
-		}
-	}
+	while (++i < bg->height)
+		write_str_nl(bg->matrix[i]);
 }
 
-void	print_tab(t_env *env)
+int	main(int argc, char **argv)
 {
-	int	i;
+	t_bg	bg;
 
-	i = 0;
-	while (env->draw.matrice[i])
-	{
-		ft_putstr(env->draw.matrice[i++], env->draw.width);
-		ft_putstr("\n", 1);
-	}
-}
-
-int	pars(FILE *fd, t_env *env)
-{
-	int		ret;
-	char	*str;
-	int		i;
-	int		j;
-
-	str = NULL;
-	ret = fscanf(fd, "%[^\n]", &str);
-	printf("str:%s\n", str);
-	if (ret != 3 || env->draw.width < 1 || env->draw.width > 300 || env->draw.height < 1 || env->draw.height > 300)
-	{	
-		ft_putstr("bug fscanf\n", ft_strlen("bug fscanf\n"));
-		return (1);
-	}
-	env->draw.matrice = malloc(sizeof(char *) * (env->draw.height + 1));
-	if (!env->draw.matrice)
-	{
-		ft_putstr("bug matrice\n", ft_strlen("bug matrice\n"));
-		return (1);
-	}
-	i = 0;
-	while (i < env->draw.height)
-	{
-		env->draw.matrice[i] = malloc(sizeof(char) * (env->draw.width + 1));
-		if (!env->draw.matrice[i])
-		{
-			ft_putstr("bug matrice[i]\n", ft_strlen("bug matrice[i]\n"));
-			return (1);
-		}
-		i++;
-	}
-	env->draw.matrice[i] = NULL;
-	i = 0;
-	ft_putstr("start\n", ft_strlen("start\n"));
-	while (i < env->draw.height)
-	{
-		j = 0;
-		while (j < env->draw.width)
-			env->draw.matrice[i][j++] = env->draw.c;
-		env->draw.matrice[i][j] = '\0';
-		i++;
-	}
-	ret = 5;
-	while ((ret = fscanf(fd, "%c %f %f %f %c", &env->obj.type, &env->obj.x, &env->obj.y, &env->obj.radius, &env->obj.color)) != EOF)
-	{
-		if (ret != 5)
-		{
-			printf("ret = %d: bug\n", ret);
-			printf("type = %c\n", env->obj.type);
-			return (1);
-		}
-		if (env->obj.radius <= 0 || (env->obj.type != 'C' && env->obj.type != 'c'))
-		{
-			ft_putstr("bug obj\n", ft_strlen("bug obj\n"));
-			return (1);
-		}
-		put_obj(env);
-	}
-	print_tab(env);
-	return (0);
-}
-
-int main(int ac, char **av)
-{
-	FILE	*fd;
-	t_env	env;
-	int		err;
-
-	if (ac != 2)
-	{
-		ft_putstr("Error: argument", ft_strlen("Error: argument"));
-		return (1);
-	}
-	fd = fopen(av[1], "r");
-	if (!fd)
-	{
-		ft_putstr("Error: Operation file corrupted", ft_strlen("Error: Operation file corrupted"));
-		return (1);
-	}
-	ft_putstr("in\n", ft_strlen("in\n"));
-	err = pars(fd, &env);
-	if (err == 1)
-	{
-		ft_putstr("Error: err = 1\n", ft_strlen("Error: err = 1\n"));
-		return (1);
-	}
-	return (0);
+	memset(&bg, 0, sizeof(t_bg));
+	if (argc != 2)
+		return(my_perror(&bg, "Error: argument"));
+	bg.file = fopen(argv[1], "r");
+	if (!bg.file)
+		return (my_perror(&bg, "Error: Operation file corrupted"));
+	if (parse_bg(&bg))
+		return (my_perror(&bg, "Error: Operation file corrupted"));
+	if (parse_draw_circles(&bg))
+		return (my_perror(&bg, "Error: Operation file corrupted"));
+	print_result(&bg);
+	clear_data(&bg);
 }
